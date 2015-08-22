f3b9620db90126faa26ed01426ddead7346faaad
Umer Hassan <umerhassan94@gmail.com>
2015-08-22 22:07:51 +0500

    App without routing is done

---
+++ js/controllers.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Umer Hassan on 8/22/2015.
+ */
+
+
+var myApp = angular.module('myApp', []);
+myApp.controller('myController', ['$scope', '$http', function ($scope, $http) {
+    $http.get('js/data.json').success(function (data) {
+        $scope.artists = data;
+    });
+}]);

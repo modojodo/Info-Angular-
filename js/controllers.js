11d506fc0f59ea2440b5dbc361516f11bf8d6012
Umer Hassan <umerhassan94@gmail.com>
2015-08-22 22:56:48 +0500

    Added filters

--- js/controllers.js
+++ js/controllers.js
@@ -7,5 +7,6 @@ var myApp = angular.module('myApp', []);
 myApp.controller('myController', ['$scope', '$http', function ($scope, $http) {
     $http.get('js/data.json').success(function (data) {
         $scope.artists = data;
+        $scope.artistOrder = 'name';
     });
 }]);
